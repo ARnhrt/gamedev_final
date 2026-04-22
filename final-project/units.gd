f8156b7a23799d587a1522ac36ac9cbbd5ca2dcf
Sabaroche <S567597@nwmissouri.edu>
2026-04-22 12:48:30 -0500

    units can be moved

--- final-project/units.gd
+++ final-project/units.gd
@@ -25,3 +25,7 @@ func set_selected(selected: bool) -> void:
 		scale = Vector2(1.1, 1.1)
 	else:
 		scale = Vector2(1.0, 1.0)
+		
+func move_to_tile(new_grid_position: Vector2i) -> void:
+	grid_position = new_grid_position
+	snap_to_grid()
